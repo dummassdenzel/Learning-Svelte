bb59a0c215ec716fb98edd370de9ca0c246f9e99
Denzel Manz <114593274+dummassdenzel@users.noreply.github.com>
2024-09-13 00:24:41 +0800

    Add Navbars and other pages

--- src/routes/hello-world/+page.svelte
+++ src/routes/hello-world/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { Alert } from 'flowbite-svelte';
+    import { InfoCircleSolid } from 'flowbite-svelte-icons';
     import { page } from '$app/stores';
     import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
   $: activeUrl = $page.url.pathname;
@@ -7,14 +9,14 @@
 
 <Navbar>
     <NavBrand href="/">
-      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Denzel Manz Perez</span>
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Denzel Manz Perez</span>
     </NavBrand>
     <NavHamburger />
     <NavUl {activeUrl}>
         <NavLi href="/home">Home</NavLi>
+        <NavLi href="../">Default</NavLi>
         <NavLi href="/hello-world">Hello World</NavLi>
     </NavUl>
 </Navbar>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p class="text-blue-500">If this is blue, tailwind works.</p>
+
+    <h1 class="font-bold text-center mt-6">Hello World!</h1>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
     import { Alert } from 'flowbite-svelte';
+    import { InfoCircleSolid } from 'flowbite-svelte-icons';
+    import { page } from '$app/stores';
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+  $: activeUrl = $page.url.pathname;
 </script>
 
-<p>Hello</p>
 
-<Alert>
-    <span class="font-medium">Welcome to the Home page!</span>
-</Alert>
+<Navbar>
+    <NavBrand href="/">
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Denzel Manz Perez</span>
+    </NavBrand>
+    <NavHamburger />
+    <NavUl {activeUrl}>
+        <NavLi href="/home">Home</NavLi>
+        <NavLi href="../">Default</NavLi>
+        <NavLi href="/hello-world">Hello World</NavLi>
+    </NavUl>
+</Navbar>
+
+    <Alert>
+        <InfoCircleSolid slot='icon'/>
+        <span class="font-medium">Welcome to the Home page!</span>
+    </Alert>
+
+    <h1 class="font-bold text-center mt-6">You are in the Home page</h1>
