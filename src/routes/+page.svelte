ec96a8fb7a966504adf08bad047d97621e778e3a
Denzel Manz <114593274+dummassdenzel@users.noreply.github.com>
2024-09-12 19:31:08 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p class="text-blue-500">If this is blue, tailwind works.</p>
+
+<script>
+    import { Alert } from 'flowbite-svelte';
+  </script>
+  
+  <Alert>
+    <span class="font-medium">Default alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+  <Alert color="blue">
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+  <Alert color="red">
+    <span class="font-medium">Danger alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+  <Alert color="green">
+    <span class="font-medium">Success alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+  <Alert color="yellow">
+    <span class="font-medium">Warning alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+  <Alert color="dark">
+    <span class="font-medium">Dark alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { Alert } from 'flowbite-svelte';
+</script>
+
+<p>Hello</p>
+
+<Alert>
+    <span class="font-medium">Welcome to the Home page!</span>
+</Alert>
